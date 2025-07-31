339406a5d3be699f5fe1170be1e01e2b08b2cf15
Akintayo Popoola <akinpope25@gmail.com>
2025-07-31 17:18:23 -0500

    Add category filter and pagination to storefront products listing

--- app/controllers/storefront/products_controller.rb
+++ app/controllers/storefront/products_controller.rb
@@ -1,7 +1,12 @@
 module Storefront
   class ProductsController < ApplicationController
     def index
-      @products = Product.includes(:category).all
+      @categories = Category.all
+      @selected_category = params[:category_id]
+
+      @products = Product.includes(:category)
+      @products = @products.where(category_id: @selected_category) if @selected_category.present?
+      @products = @products.page(params[:page]).per(6)
     end
 
     def show
@@ -9,3 +14,4 @@ module Storefront
     end
   end
 end
+
