2006c3b3b256778d18f8f45a9193b1dcd3a53138
Akintayo Popoola <akinpope25@gmail.com>
2025-08-01 14:46:39 -0500

    Add filters for New, On Sale, and Recently Updated products

--- app/controllers/storefront/products_controller.rb
+++ app/controllers/storefront/products_controller.rb
@@ -1,18 +1,28 @@
+# app/controllers/storefront/products_controller.rb
 module Storefront
   class ProductsController < ApplicationController
     def index
       @categories = Category.all
       @products = Product.includes(:category)
 
-      if params[:category_id].present?
-        @products = @products.where(category_id: params[:category_id])
-      end
-
       if params[:search].present?
         @products = @products.where("name ILIKE ?", "%#{params[:search]}%")
       end
 
-      @products = @products.page(params[:page]).per(2)
+      if params[:category_id].present?
+        @products = @products.where(category_id: params[:category_id])
+      end
+
+      case params[:filter]
+      when 'new'
+        @products = @products.order(created_at: :desc)
+      when 'sale'
+        @products = @products.where("sale_price IS NOT NULL AND sale_price < price")
+      when 'recent'
+        @products = @products.order(updated_at: :desc)
+      end
+
+      @products = @products.page(params[:page]).per(6)
     end
 
     def show
