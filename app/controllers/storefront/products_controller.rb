51d14331269733b2fec3e44ae63c521ce481130c
Akintayo Popoola <akinpope25@gmail.com>
2025-08-01 14:42:37 -0500

    Fix syntax error in ProductsController and implement search & category filtering

--- app/controllers/storefront/products_controller.rb
+++ app/controllers/storefront/products_controller.rb
@@ -2,11 +2,17 @@ module Storefront
   class ProductsController < ApplicationController
     def index
       @categories = Category.all
-      @selected_category = params[:category_id]
-
       @products = Product.includes(:category)
-      @products = @products.where(category_id: @selected_category) if @selected_category.present?
-      @products = @products.page(params[:page]).per(6)
+
+      if params[:category_id].present?
+        @products = @products.where(category_id: params[:category_id])
+      end
+
+      if params[:search].present?
+        @products = @products.where("name ILIKE ?", "%#{params[:search]}%")
+      end
+
+      @products = @products.page(params[:page]).per(2)
     end
 
     def show
@@ -14,4 +20,3 @@ module Storefront
     end
   end
 end
-
