535599b7487381f6981177f95f21c2cac1a0651f
Akintayo Popoola <akinpope25@gmail.com>
2025-08-09 18:42:12 -0500

    security: read Stripe keys from ENV only

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,6 @@
 # config/initializers/stripe.rb
-
-# config/initializers/stripe.rb
-Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-
+Stripe.api_key = ENV.fetch("STRIPE_SECRET_KEY") # do not hardcode
 Rails.configuration.stripe = {
   publishable_key: ENV["STRIPE_PUBLISHABLE_KEY"],
   secret_key:      ENV["STRIPE_SECRET_KEY"]
 }
-
-if Stripe.api_key.to_s.strip.empty?
-  Rails.logger.warn "[Stripe] Missing STRIPE_SECRET_KEY env var"
-end
