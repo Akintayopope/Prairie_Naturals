0acbe0ee3b1da45457f3dfe5da03bc9df5a1805e
Akintayo Popoola <akinpope25@gmail.com>
2025-08-09 14:34:47 -0500

    admin(categories): use FriendlyId find; show products panel with thumbnails

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,21 +1,36 @@
 ActiveAdmin.register Category do
   permit_params :name
 
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
+
   index do
     selectable_column
     id_column
     column :name
-    column :created_at
+    column :products_count if Category.column_names.include?("products_count")
     actions
   end
 
-  filter :name
-  filter :created_at
-
-  form do |f|
-    f.inputs do
-      f.input :name
+  show do
+    attributes_table do
+      row :name
+      row :slug
+      row :created_at
+      row :updated_at
     end
-    f.actions
+    panel "Products" do
+      table_for resource.products.limit(50) do
+        column(:name) { |p| link_to p.name, admin_product_path(p) }
+        column :price
+        column :stock
+        column "Image" do |p|
+          image_tag url_for(p.images.first.variant(resize_to_limit: [60, 60])) if p.images.attached?
+        end
+      end
+    end
   end
 end
